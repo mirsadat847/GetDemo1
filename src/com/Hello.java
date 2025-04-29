382628b4c8f37740cb42e3b6ba94a24314fbb3ef
mirsadat847 <osadat847@gmail.com>
2025-04-29 10:11:55 -0400

    My another commit

--- src/com/Hello.java
+++ src/com/Hello.java
@@ -5,7 +5,7 @@ public class Hello {
 	public static void main(String[] args) {
 		System.out.println("This is My Statment ");
 		System.out.println("Another commit from Java terminal.");
-
+		System.out.println("Another commit from my side to practice.");
 	}
 
 }
